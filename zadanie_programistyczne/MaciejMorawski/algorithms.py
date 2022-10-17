dc5de0e3814bbd4dd5211239af7103298bd4f6c2
maciej.morawski <62252131+mpfmorawski@users.noreply.github.com>
2022-10-18 01:13:20 +0200

    NetworkX implementation added

--- zadanie_programistyczne/MaciejMorawski/algorithms.py
+++ zadanie_programistyczne/MaciejMorawski/algorithms.py
@@ -1,12 +1,14 @@
-import networkx
+import networkx as nx
 
 
 class SolutionUsingNetworkX:
     def __init__(self, graph):
-        self._graph = graph
+        self._graph = nx.Graph()
+        self._graph.add_weighted_edges_from(
+            [(key[0], key[1], value) for key, value in graph.items()])
 
     def shortest_path(self, source, target):
-        return f"{source}->...->{target}"
+        return nx.shortest_path(self._graph, source, target, weight='weight')
 
 
 class CustomSolution:
--- zadanie_programistyczne/MaciejMorawski/main.py
+++ zadanie_programistyczne/MaciejMorawski/main.py
@@ -2,19 +2,19 @@ import json
 import ast
 
 from algorithms import SolutionUsingNetworkX
-from algorithms import CustomSolution 
+from algorithms import CustomSolution
 
 def read_graph_from_file(filename):
-    with open(filename) as json_file:
+    with open(filename,encoding='utf-8') as json_file:
         raw_data = json.load(json_file)
     return {ast.literal_eval(key): value for key, value in raw_data.items()}
 
 
 if __name__ == '__main__':
     graph = read_graph_from_file(filename="graph.json")
-    
+
     networkx_algorithm = SolutionUsingNetworkX(graph)
-    print(networkx_algorithm.shortest_path("A","B"))
+    print(f"NetworkX solution path: {networkx_algorithm.shortest_path(source = 'A', target = 'B')}")
 
     custom_algorithm = CustomSolution(graph)
-    print(custom_algorithm.shortest_path("A","B"))
+    print(f"Custom implementation of Dijkstra\'s algorithm solution path: {custom_algorithm.shortest_path(source = 'A', target = 'B')}")
