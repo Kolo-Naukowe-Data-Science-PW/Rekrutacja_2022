e04937359113e5e3a89a2afa211a5cdc98b4d0b2
maciej.morawski <62252131+mpfmorawski@users.noreply.github.com>
2022-10-18 11:44:47 +0200

    Custom implementation added

--- zadanie_programistyczne/MaciejMorawski/main.py
+++ zadanie_programistyczne/MaciejMorawski/main.py
@@ -1,11 +1,11 @@
-import json
 import ast
+import json
 
-from algorithms import SolutionUsingNetworkX
-from algorithms import CustomSolution
+from algorithms import CustomSolution, SolutionUsingNetworkX
+
 
 def read_graph_from_file(filename):
-    with open(filename,encoding='utf-8') as json_file:
+    with open(filename, encoding='utf-8') as json_file:
         raw_data = json.load(json_file)
     return {ast.literal_eval(key): value for key, value in raw_data.items()}
 
@@ -14,7 +14,9 @@ if __name__ == '__main__':
     graph = read_graph_from_file(filename="graph.json")
 
     networkx_algorithm = SolutionUsingNetworkX(graph)
-    print(f"NetworkX solution path: {networkx_algorithm.shortest_path(source = 'A', target = 'B')}")
+    print(f"NetworkX solution path: "\
+        f"{networkx_algorithm.shortest_path(source = 'A', target = 'B')}")
 
     custom_algorithm = CustomSolution(graph)
-    print(f"Custom implementation of Dijkstra\'s algorithm solution path: {custom_algorithm.shortest_path(source = 'A', target = 'B')}")
+    print(f"Custom implementation of Dijkstra\'s algorithm solution path: " \
+        f"{custom_algorithm.shortest_path(source = 'A', target = 'B')}")
